bb9c5b45bfbc709de1cd91dc74c63137fdd85599
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-19 09:12:04 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,2 +1,13 @@
-TEST(MonitorTest, PulseRateCheck) { /* ... */ }
-TEST(MonitorTest, SpO2Check) { /* ... */ }
+#pragma once
+#include <string>
+#include <functional>
+
+struct VitalCheck {
+    std::string name;
+    float value;
+    float min;
+    float max;
+};
+
+int areAllVitalsNormal(float temperature, float pulseRate, float spo2,
+             std::function<void(const std::string&)> alert = nullptr);
