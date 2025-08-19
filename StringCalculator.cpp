d8ca868eea4e0843156f895f69a37f37af8439ae
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-19 08:57:00 +0530

    Create StringCalculator.cpp

---
+++ StringCalculator.cpp
@@ -0,0 +1,8 @@
+#include "StringCalculator.h"
+
+int StringCalculator::Add(const std::string& numbers) {
+    if (numbers.empty()) {
+        return 0;
+    }
+    return 0; // temporary return to pass test
+}
