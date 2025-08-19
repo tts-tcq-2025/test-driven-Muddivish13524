8348f95b4197bf1bb41d8d887e59756773b5eafa
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-19 10:08:13 +0530

    Update and rename StringCalculator.cpp to vitals.cpp

--- StringCalculator.cpp
+++
@@ -1,8 +0,0 @@
-#include "StringCalculator.h"
-
-int StringCalculator::Add(const std::string& numbers) {
-    if (numbers.empty()) {
-        return 0;
-    }
-    return 0; // temporary return to pass test
-}
