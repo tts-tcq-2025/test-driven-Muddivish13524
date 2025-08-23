f94e68579b12877864edb309d794597df7855911
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-23 14:01:36 +0530

    Update vitals.h

--- vitals.h
+++ vitals.h
@@ -1,17 +1,1 @@
-#pragma once
 
-enum VitalCondition {
-    CRITICAL_LOW, WARNING_LOW, NORMAL, WARNING_HIGH, CRITICAL_HIGH
-};
-
-struct VitalBoundary {
-    float min;
-    float max;
-    float tolerance;
-    const char* messages[5];
-};
-
-VitalCondition mapToCondition(float value, const VitalBoundary& boundary);
-const char* conditionToMessage(VitalCondition cond, const VitalBoundary& boundary);
-bool isCritical(VitalCondition cond);
-bool overallVitalsOk(VitalCondition temp, VitalCondition pulse, VitalCondition spo2);
