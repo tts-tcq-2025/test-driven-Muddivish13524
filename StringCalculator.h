3f5fcc53cab91d8979a4c96edbd174f4585df1a4
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-09-24 09:02:13 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,11 +1,23 @@
-#ifndef STRINGCALCULATOR_H
-#define STRINGCALCULATOR_H
-
+#pragma once
 #include <string>
+#include <vector>
 
 class StringCalculator {
-public:
-    static int add(const std::string& numbers);
-};
+ public:
+    int Add(const std::string& numbers);
 
-#endif // STRINGCALCULATOR_H
+ private:
+    std::string parseDelimiter(const std::string& numbers);
+    bool hasCustomDelimiterFormat(const std::string& numbers);
+    std::string extractNumbers(const std::string& numbers);
+    std::string extractNumbersAfterDelimiterHeader(const std::string& numbers);
+    std::string extractDelimiterFromHeader(const std::string& delimiterPart);
+    bool hasBracketFormat(const std::string& delimiterPart);
+    std::string normalizeDelimiters(const std::string& numbersPart, const std::string& delimiter);
+    std::string replaceDelimiterWithComma(const std::string& text, const std::string& delimiter);
+    std::vector<int> parseNumbers(const std::string& normalizedNumbers);
+    int calculateSum(const std::vector<int>& nums);
+    void validateNumbers(const std::vector<int>& nums);
+    std::vector<int> findNegativeNumbers(const std::vector<int>& nums);
+    std::string buildNegativeNumbersMessage(const std::vector<int>& negatives);
+};
